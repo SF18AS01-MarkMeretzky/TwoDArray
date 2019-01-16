987ea96b4fe34bbdbf6de92eff088fec58e9be99
Mark Meretzky <mark.meretzky@gmail.com>
2019-01-15 21:30:17 -0500

    Update ViewController.swift

--- TwoDArray/ViewController.swift
+++ TwoDArray/ViewController.swift
@@ -4,28 +4,33 @@
 //
 //  Created by Instructor on 1/15/19.
 //  Copyright © 2019 Instructor. All rights reserved.
+//  Let the user input the 8 numbers in an array of arrays.  Think of this array
+//  as a two-dimensoinal array of 4 rows and 2 columns.
 //
 
-import UIKit
+import UIKit;
 
 class ViewController: UIViewController {
     
-    //a is a big array that will contain 4 smaller arrays.
-    //These 4 smaller arrays will be the 4 rows.
-    var a: [[Int]] = [[Int]](); //born empty
-    
+    //We know the number of rowns and columns in advance.
     let nrows: Int = 4;
     let ncols: Int = 2;
     
+    //We are currently up to this element.
     var row: Int = 0;
     var col: Int = 0;
+    
+    //a is a big array, born empty, that will contain four smaller arrays.
+    //These four smaller arrays will be the four rows.
+    var a: [[Int]] = [[Int]]();
 
     @IBOutlet weak var textField: UITextField!;
     
     override func viewDidLoad() {
         super.viewDidLoad();
         // Do any additional setup after loading the view, typically from a nib.
-        //insert an empty array of Ints into the big array.
+        
+        //Insert an empty array of Ints into the big array.
         //This empty array of Ints will become the first row.
         a.append([Int]());
         textField.placeholder = "Please type row \(row + 1), col \(col + 1):";
@@ -34,31 +39,39 @@ class ViewController: UIViewController {
     @IBAction func returnKeyPressed(_ sender: UITextField) {
         if let text: String = sender.text {
             if let number: Int = Int(text) {
-                a[row].append(number);
+                a[row].append(number); //Arrive here if the user actually typed a number.
+                
+                //Move to the next position in the two-dimensional array.
                 col += 1;
                 if col == ncols {
                     col = 0;
                     row += 1;
-                    a.append([Int]());  //Start another row.
                     if row == nrows {
-                        //All done!
-                        sender.resignFirstResponder();
-                        sender.isEnabled = false;
-                        for row in a {
-                            for i in row {
-                                print("\(i) ", terminator: "");
-                            }
-                            print();
-                        }
-                        textField.text = nil;
-                        textField.placeholder = nil;
+                        allDone();
                         return;
-                    } 
+                    }
+                    a.append([Int]()); //Start another row.
                 }
-                textField.text = nil; //allow the placeholder to appear.
+
+                textField.text = nil;  //Allow the placeholder to appear.
                 textField.placeholder = "Please type row \(row + 1), col \(col + 1):";
             }
         }
     }
-}
+    
+    //Arrive here when the user has finished inputting all the numbers.
+    
+    func allDone() {
+        textField.resignFirstResponder();
+        textField.isEnabled = false;
+        textField.text = nil;
+        textField.placeholder = nil;
 
+        for row in a {
+            for i in row {
+                print("\(i) ", terminator: "");
+            }
+            print();   //Move down to the next line.
+        }
+    }
+}
